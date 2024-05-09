4ea36b9fb6527514c7618971d20a394e5ff2b05f
Fadi <fadinachawati17@gmail.com>
2024-05-09 12:50:44 +0200

    CAN CRC

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import ModbusCrc from "./components/ModbusCrc.jsx";
+import CanCrc from "./components/CanCrc.jsx";
 
 const App = () => {
     return(
         <>
-            <ModbusCrc/>
+            {/*<ModbusCrc/>*/}
+            <CanCrc/>
         </>
     )
 }
