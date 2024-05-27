f59c447e9ee4d2def25340b8d94221ec1b803d1e
Fadi <fadinachawati17@gmail.com>
2024-05-27 18:12:47 +0200

    Revert "conditionings - can-crc"
    This reverts commit 7a6b489a82413ccf0cd60958d22d9f1114ce6a7e.

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const InfoCard = ({label, value}) => {
     return (
-        <div className="flex-1 mt-6 p-6 bg-white border border-gray-200 rounded-2xl shadow lg:mt-12">
+        <div className="flex-1 mt-12 p-6 bg-white border border-gray-200 rounded-2xl shadow">
             <label className="text-sm font-medium text-gray-900">
                 {label}
             </label>
