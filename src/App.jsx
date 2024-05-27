7a6b489a82413ccf0cd60958d22d9f1114ce6a7e
Fadi <fadinachawati17@gmail.com>
2024-05-27 17:59:51 +0200

    conditionings - can-crc

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import ModbusCrc from "./components/ModbusCrc.jsx";
 import CanCrc from "./components/CanCrc.jsx";
+import {Toaster} from "react-hot-toast";
 
 const App = () => {
     return(
         <>
             {/*<ModbusCrc/>*/}
             <CanCrc/>
+            <Toaster
+                position="bottom-left"
+                reverseOrder={false}
+            />
         </>
     )
 }
