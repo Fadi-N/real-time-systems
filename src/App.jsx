2eccedb827f6b9e93f7d849db3a774bebfd17ec0
Fadi <fadinachawati17@gmail.com>
2024-05-27 18:28:14 +0200

    conditionings

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import './App.css'
-import ModbusCrc from "./components/ModbusCrc.jsx";
 import CanCrc from "./components/CanCrc.jsx";
+import {Toaster} from "react-hot-toast";
 
-import CanCrc from "./components/CanCrc.jsx";
-import Ieee754 from "./components/Ieee754.jsx";
 
 
 const App = () => {
     return(
         <>
             {/*<ModbusCrc/>*/}
-            {/*<CanCrc/>*/}
+            <CanCrc/>
             <Toaster
                 position="bottom-left"
                 reverseOrder={false}
             />
-            <CanCrc/>
         </>
     )
 }
