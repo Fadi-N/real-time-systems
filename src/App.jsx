ac6b22f2491a8c2ea715473acade1788ff50afb4
Fadi <fadinachawati17@gmail.com>
2024-05-27 17:31:30 +0200

    conditionings

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import ModbusCrc from "./components/ModbusCrc.jsx";
+import {Toaster} from "react-hot-toast";
 
 const App = () => {
     return(
         <>
             <ModbusCrc/>
+            <Toaster
+                position="bottom-left"
+                reverseOrder={false}
+            />
         </>
     )
 }
